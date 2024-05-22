42e09f1f58cf766ace80d5309cac964388f38d82
geoffrey lecluse <geoffrey.lecluse45@gmail.com>
2024-05-22 19:05:34 +0200

    maj

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import pandas as pd
 import streamlit as st
+from genere import generer
+import os
+
+file_path = 'contacts_safti_csv.csv'
+
+if not os.path.exists(file_path):
+    generer()
 
 df = pd.read_csv('contacts_safti_csv.csv')
-df.head()
+df=df.drop(['photo', 'minisite'], axis=1)
+
+regions = list(df['region'].unique())
+departements = list(df['département'].unique())
+regions.insert(0,"Toutes les régions")
+choix = st.sidebar.selectbox("Choisir une région", regions)
+df = df.drop('Unnamed: 0', axis=1)
+
+st.write(f"Sélection : {choix}")
+
+if choix == "Toutes les régions":
+    st.write(df.to_html(index=False), unsafe_allow_html=True)
+else :
+    df=df[df['region']==choix]
+    st.write(df.to_html(index=False), unsafe_allow_html=True)
